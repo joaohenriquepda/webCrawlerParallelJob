7875571eb2f45855fd58f1e0653811c177a3e219
João Henrique <joaohenrique.p.almeida@gmail.com>
2018-04-28 22:37:52 -0300

    Adding config for Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+
+  #route for sidekiq list jobs
+  mount Sidekiq::Web => '/sidekiq'
+  resources :pages
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
